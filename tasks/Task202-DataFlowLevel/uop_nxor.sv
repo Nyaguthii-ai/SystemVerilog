87d0fb535d5b7c7a1c23b3824acde045dea6ef2c
Nyaguthii-ai <71819669+Nyaguthii-ai@users.noreply.github.com>
2022-02-04 12:56:17 +0000

    tasks

--- tasks/Task202-DataFlowLevel/uop_nxor.sv
+++ tasks/Task202-DataFlowLevel/uop_nxor.sv
@@ -1,13 +1,13 @@
 module uop_nxor (output wire Y, input wire A, input wire B);
 
 //Internal wires
-wire term0;
-wire term3;
+//wire term0;
+//wire term3;
 
 //Continuous Assignment (order does not matter)
-assign Y = term0 | term3;
-assign term0 = ~A & ~B;
-assign term3 = A & B;
+assign Y = ~A & ~B|A & B;
+//assign term0 = ~A & ~B;
+//assign term3 = A & B;
 
 //TASK - can you remove term0 and term3 and write everthing on one line?
 
